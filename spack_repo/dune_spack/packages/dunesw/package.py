6a2432d99443dc57281f66fa2cbb52587e1511ed
v hewes <vhewes@fnal.gov>
2026-08-11 13:42:18 -0500

    remove dune-pardata package dependency

--- spack_repo/dune_spack/packages/dunesw/package.py
+++ spack_repo/dune_spack/packages/dunesw/package.py
@@ -65,7 +65,6 @@ class Dunesw(CMakePackage):
     depends_on("cmake", type="build")
     depends_on("justin", type="run")
     depends_on("larg4", type=("test","run"))
-    depends_on("dune-pardata", type=("test","run"))
 
     def cmake_args(self):
         args = [
